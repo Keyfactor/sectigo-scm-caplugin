fb85f345cd59ed5b3d3b00abfe81a81d4104de8b
Dave Galey <89407235+dgaley@users.noreply.github.com>
2024-08-14 11:56:54 -0400

    Initial (#1)
    * initial release
    
    * Create keyfactor-bootstrap-workflow-v3.yml
    
    * doc
    
    * more doc
    
    * manifest
    
    * plugin manifest
    
    * move configuration.md file
    
    * copy manifest to output dir
    
    * test
    
    * move client creation
    
    * more test
    
    * test fix
    
    * more fix
    
    * copy packages locally
    
    * Update generated docs
    
    * cert auth fix
    
    * changelog and manifest
    
    ---------
    
    Co-authored-by: Keyfactor <keyfactor@keyfactor.github.io>

---
+++ sectigo-scm-caplugin/Models/CustomField.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keyfactor.Extensions.CAPlugin.Sectigo.Models
+{
+	public class CustomField
+	{
+		public string name { get; set; }
+
+		public string value { get; set; }
+
+		public bool mandatory { get; set; }
+	}
+}
---
+++ sectigo-scm-caplugin/SectigoConfig.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keyfactor.Extensions.CAPlugin.Sectigo
+{
+	public class SectigoConfig
+	{
+		public SectigoConfig()
+		{
+			
+		}
+
+		[JsonProperty("ApiEndpoint")]
+		public string ApiEndpoint { get; set; }
+
+		[JsonProperty("AuthType")]
+		public string AuthenticationType { get; set; }
+
+		[JsonProperty("CustomerUri")]
+		public string CustomerUri { get; set; }
+
+		[JsonProperty("Username")]
+		public string Username { get; set; }
+
+		[JsonProperty("Password")]
+		public string Password { get; set; }
+
+		[JsonProperty("PickupRetries")]
+		public int PickupRetries { get; set; }
+
+		[JsonProperty("PickupDelay")]
+		public int PickupDelayInSeconds { get; set; }
+
+		[JsonProperty("PageSize")]
+		public int PageSize { get; set; }
+
+		[JsonProperty("ExternalRequestorFieldName")]
+		public string ExternalRequestorFieldName { get; set; }
+
+		[JsonProperty("SyncFilterProfileId")]
+		public string SyncFilterProfileId { get; set; }
+
+		[JsonProperty("ForceCompleteSync", DefaultValueHandling = DefaultValueHandling.Populate)]
+		public bool ForceCompleteSync { get; set; } = false;
+
+		[JsonProperty("Enabled")]
+		public bool Enabled { get; set; } = true;
+
+		[JsonProperty("ClientCertificate")]
+		public ClientCertificate Certificate { get; set; }
+	}
+
+	public class ClientCertificate
+	{
+		public string StoreName { get; set; }
+		public string StoreLocation { get; set; }
+		public string Thumbprint { get; set; }
+		public string CertificatePath { get; set; }
+		public string CertificatePassword { get; set; }
+	}
+}
